c681627caef0681a3cfddaf461180a6667dc0779
Omar Ansari <omaransari1@gmail.com>
2017-04-29 21:30:33 -0500

    Change post object to hash and create functions to 'Add new Post' and 'incrementUpvote' by 1

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,10 +2,22 @@
 angular.module('redditesque', []).controller('mainCtrl', ['$scope', function($scope) {
   $scope.test = "Hello world";
   $scope.posts = [
-    'post1',
-    'post2',
-    'post3',
-    'post4',
-    'post5'
+    {title:'post1', upvotes: 2},
+    {title:'post2', upvotes: 8},
+    {title:'post3', upvotes: 5},
+    {title:'post4', upvotes: 12},
+    {title:'post5', upvotes: 9}
   ];
+
+  $scope.addPost = function() {
+    if(!$scope.title || $scope.title === '') {
+      return;
+    }
+    $scope.posts.push({title: $scope.title, upvotes: 0});
+    $scope.title = '';
+  };
+
+  $scope.incrementUpvotes = function(post) {
+    post.upvotes += 1;
+  };
 }]);
