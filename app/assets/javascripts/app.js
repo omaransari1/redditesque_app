af5b23aa06df1afbd7f2e4d346a95c5a2c9c85ef
Omar Ansari <omaransari1@gmail.com>
2017-05-01 02:08:47 -0500

    Add config block to set home state

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,6 +1,20 @@
 /* global angular */
 var app = angular.module('redditesque', ['ui.router']);
 
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider.state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'mainCtrl'
+    });
+
+    $urlRouterProvider.otherwise('home');
+  }]);
+
 app.factory('posts', [function() {
   var o = {
     posts: []
