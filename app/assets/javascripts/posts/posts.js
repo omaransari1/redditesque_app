ca3c867e68a2e777b2917bb934601999e36bd332
Omar Ansari <omaransari1@gmail.com>
2017-05-05 04:54:32 -0500

    Add function to get http response through angular to our api and copy data to our posts object in angular

--- app/assets/javascripts/posts/posts.js
+++ app/assets/javascripts/posts/posts.js
@@ -1,6 +1,12 @@
-app.factory('posts', [function() {
+app.factory('posts', ['$http',function($http) {
   var o = {
     posts: []
+  };
+
+  o.getAll = function() {
+    return $http.get('/posts.json').success(function(data) {
+      angular.copy(data, o.posts);
+    });
   };
   return o;
 }]);
